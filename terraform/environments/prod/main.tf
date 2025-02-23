c703b370498be5c118a1efe7e22bdb2f83e2f578
Contributor <contributor@example.com>
2025-02-23 13:14:56 -0800

    Add Terraform configuration for GitHub Actions OIDC and IAM role

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -1,113 +1,37 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
-
-  name = "${var.environment}-vpc"
-  cidr = var.vpc_cidr
-
-  azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
-  private_subnets = [for i in range(3) : cidrsubnet(var.vpc_cidr, 4, i)]
-  public_subnets  = [for i in range(3) : cidrsubnet(var.vpc_cidr, 4, i + 3)]
-
-  enable_nat_gateway     = true
-  single_nat_gateway     = false
-  one_nat_gateway_per_az = true
-  enable_vpn_gateway     = false
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = "1"
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = "1"
-  }
-
-  tags = var.tags
+# GitHub OIDC Provider and IAM Role
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0"
-
-  cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
-
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids              = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.private_subnets
-
-  cluster_endpoint_public_access = true
-
-  eks_managed_node_groups = {
-    for name, config in var.node_groups : name => {
-      name           = name
-      instance_types = config.instance_types
-      min_size      = config.min_size
-      max_size      = config.max_size
-      desired_size  = config.desired_size
-
-      subnet_ids = module.vpc.private_subnets
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github_actions.arn]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${var.github_org}/${var.github_repo}:*"]
     }
   }
-
-  tags = var.tags
 }
 
-module "ecr" {
-  source = "terraform-aws-modules/ecr/aws"
-  version = "~> 1.6"
-
-  repository_name = "${var.environment}-app"
-  repository_type = "private"
-
-  repository_lifecycle_policy = jsonencode({
-    rules = [
-      {
-        rulePriority = 1
-        description  = "Keep last 30 images"
-        selection = {
-          tagStatus     = "tagged"
-          tagPrefixList = ["v"]
-          countType     = "imageCountMoreThan"
-          countNumber   = 30
-        }
-        action = {
-          type = "expire"
-        }
-      }
-    ]
-  })
-
-  tags = var.tags
+resource "aws_iam_role" "github_actions" {
+  name               = "github-actions-role"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
 }
 
-# Create IAM role for Slack bot
-resource "aws_iam_role" "slackbot" {
-  name = "${var.environment}-slackbot-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-
-  tags = var.tags
+resource "aws_iam_role_policy_attachment" "github_actions_eks" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.github_actions.arn
 }
 
-# Attach policy for EKS access
-resource "aws_iam_role_policy" "slackbot_eks" {
-  name = "${var.environment}-slackbot-eks-policy"
-  role = aws_iam_role.slackbot.id
+resource "aws_iam_policy" "github_actions" {
+  name = "github-actions-policy"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -115,30 +39,19 @@ resource "aws_iam_role_policy" "slackbot_eks" {
       {
         Effect = "Allow"
         Action = [
-          "eks:DescribeCluster",
-          "eks:ListClusters"
+          "eks:*",
+          "ecr:*",
+          "iam:*",
+          "vpc:*",
+          "ec2:*"
         ]
-        Resource = module.eks.cluster_arn
+        Resource = "*"
       }
     ]
   })
 }
 
-# Create Lambda function for Slack bot (placeholder - will be implemented later)
-resource "aws_lambda_function" "slackbot" {
-  filename         = "slackbot.zip"  # This will be created by CI/CD
-  function_name    = "${var.environment}-slackbot"
-  role            = aws_iam_role.slackbot.arn
-  handler         = "index.handler"
-  runtime         = "nodejs18.x"
-  timeout         = 30
-
-  environment {
-    variables = {
-      CLUSTER_NAME = var.cluster_name
-      REGION      = var.aws_region
-    }
-  }
-
-  tags = var.tags
-} 
+output "github_actions_role_arn" {
+  description = "ARN of the IAM role for GitHub Actions"
+  value       = aws_iam_role.github_actions.arn
+}
